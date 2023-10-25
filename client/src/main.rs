5701a46bd59a1eb89c222169d2a09fd6adf937a9
TokieSan <eltokhy@aucegypt.edu>
2023-10-25 09:31:08 +0300

    initial client-to-client implementation

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,35 +1,119 @@
 use std::io;
 use std::net::TcpStream;
-use std::io::Read;
-use std::io::Write;
+use std::io::{Read, Write};
+use std::thread;
+use chrono;
+use core::time::Duration;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
-fn main() -> io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:7878")?;
+fn receive_images_from_server(mut stream: TcpStream, client_id: usize) -> io::Result<()> {
+    let mut buf = [0; 1024 * 1024];
 
     loop {
-        // Read the image name from stdin
-        let mut image_name = String::new();
-        println!("Enter the image name (or 'exit' to quit): ");
-        io::stdin().read_line(&mut image_name)?;
-        image_name = image_name.trim().to_string();
+        let bytes_read = stream.read(&mut buf)?;
 
-        if image_name == "exit" {
-            break; // Exit the loop if 'exit' is entered
+        if bytes_read == 0 {
+            println!("Server disconnected.");
+            return Ok(());
         }
 
-        // Try to read the image file and send it to the server
-        match std::fs::read(&image_name) {
-            Ok(img_data) => {
-                stream.write(&img_data)?;
-                let mut buffer = [0; 1024 * 1024];
-                stream.read(&mut buffer)?;
-                println!("Response from server: {} bytes", buffer.len());
-            }
-            Err(e) => {
-                eprintln!("Error reading image file: {}", e);
-            }
-        }
+        let image_name = format!("received-{}.png", chrono::Utc::now().timestamp());
+
+        println!("Saving image as: {}", image_name);
+
+        std::fs::write(&image_name, &buf[..bytes_read])?;
+
+        println!("Image saved.");
+
+        thread::sleep(Duration::from_secs(5));
     }
+}
 
-    Ok(())
+fn main() -> io::Result<()> {
+    println!("Starting client...");
+
+    let server_address = "127.0.0.1:7878";
+    let mut stream = TcpStream::connect(server_address)?;
+
+    println!("Connected to server at {}", server_address);
+
+    let mut input = String::new();
+    let client_id: Option<usize> = None;
+
+    let client_id_to_stream: Arc<Mutex<HashMap<usize, TcpStream>>> =
+        Arc::new(Mutex::new(HashMap::new()));
+    client_id_to_stream.lock().unwrap().insert(0, stream.try_clone()?);
+
+    let listener_stream = stream.try_clone()?;
+    let client_id_to_stream_clone = client_id_to_stream.clone();
+    let mut listening = false;
+    let listen_thread = thread::spawn(move || {
+        let mut buf = [0; 1024 * 1024];
+        loop {
+            if listening {
+                let mut client_map = client_id_to_stream_clone.lock().unwrap();
+                for (client_id, client_stream) in client_map.iter_mut() {
+                    let bytes_read = client_stream.read(&mut buf).unwrap_or(0);
+
+                    if bytes_read > 0 {
+                        let image_name = format!("received-{}.png", chrono::Utc::now().timestamp());
+                        println!("Received image from Client {}: Saving as {}", client_id, image_name);
+                        std::fs::write(&image_name, &buf[..bytes_read]).unwrap();
+                    }
+                }
+            }
+            thread::sleep(Duration::from_secs(2));
+        }
+    });
+
+    loop {
+        println!("Enter a command (e.g., 'send <client_id> <image_name>', 'list', or 'listen'): ");
+        io::stdin().read_line(&mut input)?;
+
+        let parts: Vec<&str> = input.trim().split_whitespace().collect();
+
+        match parts[0] {
+            "send" => {
+                if parts.len() >= 3 {
+                    if let Ok(client_id) = parts[1].parse::<usize>() {
+                        let image_name = parts[2];
+                        let mut client_map = client_id_to_stream.lock().unwrap();
+                        if let Some(client_stream) = client_map.get_mut(&client_id) {
+                            let image_data = std::fs::read(image_name)?;
+                            client_stream.write(&image_data)?;
+                            println!("Image sent to Client {}.", client_id);
+                        } else {
+                            println!("Client {} not found or not connected.", client_id);
+                        }
+                    } else {
+                        println!("Invalid client ID.");
+                    }
+                } else {
+                    println!("Please specify a client ID and an image name.");
+                }
+            }
+            "list" => {
+                stream.write(b"list")?;
+                let mut response = [0; 1024];
+                let bytes_read = stream.read(&mut response)?;
+                if bytes_read > 0 {
+                    let response_str = String::from_utf8_lossy(&response[..bytes_read]);
+                    println!("Available clients and their IPs:");
+                    println!("{:<10}{}", "Client ID", "IP Address");
+                    for line in response_str.lines() {
+                        println!("{}", line);
+                    }
+                }
+            }
+            "listen" => {
+                listening = true;
+            }
+            _ => {
+                println!("Invalid command. Try 'send <client_id> <image_name>', 'list', or 'listen'.");
+            }
+        }
+
+        input.clear();
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,64 +3,67 @@ use std::time;
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::thread;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
-fn handle_client(mut stream: TcpStream) -> io::Result<()> {
+fn handle_client(
+    mut stream: TcpStream,
+    client_id: usize,
+    clients: Arc<Mutex<HashMap<usize, TcpStream>>>,
+) -> io::Result<()> {
+    println!("Client {} connected from {}.", client_id, stream.peer_addr()?);
 
-  println!("New client connected.");
-  
-  let mut buf = [0; 1024 * 1024];
-  
-  loop {
-    let bytes_read = stream.read(&mut buf)?;
+    let mut buf = [0; 1024];
+    loop {
+        let bytes_read = stream.read(&mut buf)?;
 
-    if bytes_read == 0 {
-        println!("Client disconnected.");
-        return Ok(());
+        if bytes_read == 0 {
+            println!("Client {} disconnected.", client_id);
+            clients.lock().unwrap().remove(&client_id);
+            return Ok(());
+        }
+
+        let command = String::from_utf8_lossy(&buf[..bytes_read]).trim().to_string().to_owned();
+
+        match command.as_str() {
+            "list" => {
+                let client_list = clients.lock().unwrap();
+                let client_info: Vec<String> = client_list.iter().map(|(id, client)| {
+                    format!("Client {}: {}", id, client.peer_addr().unwrap())
+                }).collect();
+                let response = client_info.join("\n");
+                stream.write(response.as_bytes())?;
+            }
+            _ => {
+                // Handle other commands or ignore unknown commands
+            }
+        }
     }
-    
-    let image_name = format!("received-{}.png", chrono::Utc::now().timestamp());
-    
-    println!("Saving image as: {}", image_name);
-    
-    std::fs::write(image_name, &buf[..bytes_read])?;
-
-    println!("Image saved.");
-
-    stream.write(&buf[..bytes_read])?;
-
-    println!("Response sent to client.");
-
-    thread::sleep(time::Duration::from_secs(5));
-  }
 }
 
 fn main() -> io::Result<()> {
+    println!("Starting server...");
 
-  println!("Starting server...");
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    let clients: Arc<Mutex<HashMap<usize, TcpStream>>> = Arc::new(Mutex::new(HashMap::new()));
+    let mut client_id = 0;
 
-  let listener = TcpListener::bind("127.0.0.1:7878")?;
+    println!("Listening for clients...");
 
-  println!("Listening for clients...");
+    for stream in listener.incoming() {
+        let stream = stream.expect("failed");
+        let clients_copy = Arc::clone(&clients);
+        clients.lock().unwrap().insert(client_id, stream.try_clone().unwrap());
 
-  let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
+        let handle = thread::spawn(move || {
+            handle_client(stream, client_id, clients_copy)
+                .unwrap_or_else(|error| eprintln!("{:?}", error));
+        });
 
-  for stream in listener.incoming() {
-    let stream = stream.expect("failed");
+        client_id += 1;
+    }
 
-    let handle = thread::spawn(move || {
-        handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
-    });
-    
-    thread_vec.push(handle);
-  }
+    println!("Shutting down server...");
 
-  println!("Shutting down server...");
-
-  for handle in thread_vec {
-    handle.join().unwrap();
-  }
-
-  println!("Server shutdown complete.");
-
-  Ok(())
+    Ok(())
 }
