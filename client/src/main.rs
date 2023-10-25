fbbf20171856791dfb64ec6bb222ab7c4e347160
TokieSan <eltokhy@aucegypt.edu>
2023-10-25 08:32:39 +0300

    made client take image from stdin & allow multisending in same session

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,18 +4,32 @@ use std::io::Read;
 use std::io::Write;
 
 fn main() -> io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:7878")?;
 
-  let mut stream = TcpStream::connect("127.0.0.1:7878")?;
-  
-  let img_data = std::fs::read("image.png")?;
+    loop {
+        // Read the image name from stdin
+        let mut image_name = String::new();
+        println!("Enter the image name (or 'exit' to quit): ");
+        io::stdin().read_line(&mut image_name)?;
+        image_name = image_name.trim().to_string();
 
-  stream.write(&img_data)?;
+        if image_name == "exit" {
+            break; // Exit the loop if 'exit' is entered
+        }
 
-  let mut buffer = [0; 1024 * 1024];
-  
-  stream.read(&mut buffer)?;
+        // Try to read the image file and send it to the server
+        match std::fs::read(&image_name) {
+            Ok(img_data) => {
+                stream.write(&img_data)?;
+                let mut buffer = [0; 1024 * 1024];
+                stream.read(&mut buffer)?;
+                println!("Response from server: {} bytes", buffer.len());
+            }
+            Err(e) => {
+                eprintln!("Error reading image file: {}", e);
+            }
+        }
+    }
 
-  println!("Response from server: {} bytes", buffer.len());
-
-  Ok(())
+    Ok(())
 }
