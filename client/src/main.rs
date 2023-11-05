6138164a1bc76d463ee1259fbd2a092f20b2dccf
TokieSan <eltokhy@aucegypt.edu>
2023-11-05 22:22:26 +0200

    converted connections to UDP

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,116 +1,98 @@
 use std::io;
 use std::net::TcpStream;
-use std::io::{Read, Write};
+use std::io::{BufRead, Read, Write, Result};
 use std::thread;
 use chrono;
 use core::time::Duration;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
 
-fn receive_images_from_server(mut stream: TcpStream, client_id: usize) -> io::Result<()> {
-    let mut buf = [0; 1024 * 1024];
+use std::fs::File;
+use std::net::UdpSocket;
+use std::env;
+use std::str;
 
-    loop {
-        let bytes_read = stream.read(&mut buf)?;
 
-        if bytes_read == 0 {
-            println!("Server disconnected.");
-            return Ok(());
-        }
 
-        let image_name = format!("received-{}.png", chrono::Utc::now().timestamp());
+//fn receive_images_from_server(mut stream: TcpStream, client_id: usize) -> io::Result<()> {
+    //let mut buf = [0; 1024 * 1024];
 
-        println!("Saving image as: {}", image_name);
+    //loop {
+        //let bytes_read = stream.read(&mut buf)?;
 
-        std::fs::write(&image_name, &buf[..bytes_read])?;
+        //if bytes_read == 0 {
+            //println!("Server disconnected.");
+            //return Ok(());
+        //}
 
-        println!("Image saved.");
+        //let image_name = format!("received-{}.png", chrono::Utc::now().timestamp());
 
-        thread::sleep(Duration::from_secs(5));
-    }
+        //println!("Saving image as: {}", image_name);
+
+        //std::fs::write(&image_name, &buf[..bytes_read])?;
+
+        //println!("Image saved.");
+
+        //thread::sleep(Duration::from_secs(5));
+    //}
+//}
+
+fn send_image_to_server(server_address: &str, image_path: &str) -> Result<()> {
+    let socket = UdpSocket::bind("0.0.0.0:0").expect("Failed to bind socket");
+    let mut file = File::open(image_path)?;
+
+    // Read the image data from the file into a buffer.
+    let mut image_data = Vec::new();
+    file.read_to_end(&mut image_data)?;
+
+    // Send the image data to the server at the specified address.
+    socket.send_to(&image_data, server_address)?;
+
+    println!("Image sent to the server.");
+    Ok(())
 }
 
 fn main() -> io::Result<()> {
     println!("Starting client...");
 
-    let server_address = "127.0.0.1:7878";
-    let mut stream = TcpStream::connect(server_address)?;
+    //let server_address = "127.0.0.1:8888";
+    let server_addresses = &["127.0.0.1:8887", "127.0.0.1:8888", "127.0.0.1:8889"]; // Replace with the server IPs.
 
-    println!("Connected to server at {}", server_address);
+    println!("Started successfully, broadcasting on port 8888");
 
     let mut input = String::new();
-    let client_id: Option<usize> = None;
-
-    let client_id_to_stream: Arc<Mutex<HashMap<usize, TcpStream>>> =
-        Arc::new(Mutex::new(HashMap::new()));
-    client_id_to_stream.lock().unwrap().insert(0, stream.try_clone()?);
-
-    let listener_stream = stream.try_clone()?;
-    let client_id_to_stream_clone = client_id_to_stream.clone();
-    let mut listening = false;
-    let listen_thread = thread::spawn(move || {
-        let mut buf = [0; 1024 * 1024];
-        loop {
-            if listening {
-                let mut client_map = client_id_to_stream_clone.lock().unwrap();
-                for (client_id, client_stream) in client_map.iter_mut() {
-                    let bytes_read = client_stream.read(&mut buf).unwrap_or(0);
-
-                    if bytes_read > 0 {
-                        let image_name = format!("received-{}.png", chrono::Utc::now().timestamp());
-                        println!("Received image from Client {}: Saving as {}", client_id, image_name);
-                        std::fs::write(&image_name, &buf[..bytes_read]).unwrap();
-                    }
-                }
-            }
-            thread::sleep(Duration::from_secs(2));
-        }
-    });
 
     loop {
-        println!("Enter a command (e.g., 'send <client_id> <image_name>', 'list', or 'listen'): ");
+        println!("Enter a command (e.g., 'send <image_name>', 'list', or 'listen'): ");
         io::stdin().read_line(&mut input)?;
 
         let parts: Vec<&str> = input.trim().split_whitespace().collect();
 
         match parts[0] {
             "send" => {
-                if parts.len() >= 3 {
-                    if let Ok(client_id) = parts[1].parse::<usize>() {
-                        let image_name = parts[2];
-                        let mut client_map = client_id_to_stream.lock().unwrap();
-                        if let Some(client_stream) = client_map.get_mut(&client_id) {
-                            let image_data = std::fs::read(image_name)?;
-                            client_stream.write(&image_data)?;
-                            println!("Image sent to Client {}.", client_id);
-                        } else {
-                            println!("Client {} not found or not connected.", client_id);
-                        }
-                    } else {
-                        println!("Invalid client ID.");
+                let image_path = parts[1];
+                for server_address in server_addresses {
+                    if let Err(err) = send_image_to_server(server_address, image_path) {
+                        eprintln!("Error sending image to the server: {}", err);
                     }
-                } else {
-                    println!("Please specify a client ID and an image name.");
                 }
             }
             "list" => {
-                stream.write(b"list")?;
-                let mut response = [0; 1024];
-                let bytes_read = stream.read(&mut response)?;
-                if bytes_read > 0 {
-                    let response_str = String::from_utf8_lossy(&response[..bytes_read]);
-                    println!("Available clients and their IPs:");
-                    println!("{:<10}{}", "Client ID", "IP Address");
-                    for line in response_str.lines() {
-                        println!("{}", line);
-                    }
-                }
+                //stream.write(b"list")?;
+                //let mut response = [0; 1024];
+                //let bytes_read = stream.read(&mut response)?;
+                //if bytes_read > 0 {
+                    //let response_str = String::from_utf8_lossy(&response[..bytes_read]);
+                    //println!("Available clients and their IPs:");
+                    //println!("{:<10}{}", "Client ID", "IP Address");
+                    //for line in response_str.lines() {
+                        //println!("{}", line);
+                    //}
+                //}
             }
             "listen" => {
-                listening = true;
             }
             _ => {
-                println!("Invalid command. Try 'send <client_id> <image_name>', 'list', or 'listen'.");
+                println!("Invalid command.");
             }
         }
 
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,68 +1,138 @@
 use std::io;
 use std::time;
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
+//use std::net::{TcpListener, TcpStream};
+use std::net::{UdpSocket, SocketAddr};
+use std::io::{Read, Write, Result};
 use std::thread;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+use std::fs::{File, create_dir_all};
+use chrono::Utc;
 
-fn handle_client(
-    mut stream: TcpStream,
-    client_id: usize,
-    clients: Arc<Mutex<HashMap<usize, TcpStream>>>,
-) -> io::Result<()> {
-    println!("Client {} connected from {}.", client_id, stream.peer_addr()?);
+//fn handle_client(
+    //mut stream: TcpStream,
+    //client_id: usize,
+    //clients: Arc<Mutex<HashMap<usize, TcpStream>>>,
+//) -> io::Result<()> {
+    //println!("Client {} connected from {}.", client_id, stream.peer_addr()?);
 
-    let mut buf = [0; 1024];
-    loop {
-        let bytes_read = stream.read(&mut buf)?;
+    //let mut buf = [0; 1024];
+    //loop {
+        //let bytes_read = stream.read(&mut buf)?;
 
-        if bytes_read == 0 {
-            println!("Client {} disconnected.", client_id);
-            clients.lock().unwrap().remove(&client_id);
-            return Ok(());
-        }
+        //if bytes_read == 0 {
+            //println!("Client {} disconnected.", client_id);
+            //clients.lock().unwrap().remove(&client_id);
+            //return Ok(());
+        //}
 
-        let command = String::from_utf8_lossy(&buf[..bytes_read]).trim().to_string().to_owned();
+        //let command = String::from_utf8_lossy(&buf[..bytes_read]).trim().to_string().to_owned();
 
-        match command.as_str() {
-            "list" => {
-                let client_list = clients.lock().unwrap();
-                let client_info: Vec<String> = client_list.iter().map(|(id, client)| {
-                    format!("Client {}: {}", id, client.peer_addr().unwrap())
-                }).collect();
-                let response = client_info.join("\n");
-                stream.write(response.as_bytes())?;
-            }
-            _ => {
-                // Handle other commands or ignore unknown commands
-            }
-        }
-    }
+        //match command.as_str() {
+            //"list" => {
+                //let client_list = clients.lock().unwrap();
+                //let client_info: Vec<String> = client_list.iter().map(|(id, client)| {
+                    //format!("Client {}: {}", id, client.peer_addr().unwrap())
+                //}).collect();
+                //let response = client_info.join("\n");
+                //stream.write(response.as_bytes())?;
+            //}
+            //_ => {
+                //// Handle other commands or ignore unknown commands
+            //}
+        //}
+    //}
+//}
+
+fn write_image_to_file(data: &[u8], client_addr: &SocketAddr) -> Result<()> {
+    // Get the current UTC date and time as a string.
+    let current_datetime = Utc::now().to_rfc3339();
+
+    // Create a file name using the client's address and the current date and time.
+    let filename = format!("received_{}_{}.jpg", client_addr, current_datetime);
+    let filename_clone = filename.clone();
+
+    // Create and write the image data to the file.
+    let mut file = File::create(filename)?;
+    file.write_all(data)?;
+
+    println!("Image saved as: {}", filename_clone);
+    Ok(())
 }
 
 fn main() -> io::Result<()> {
     println!("Starting server...");
 
-    let listener = TcpListener::bind("127.0.0.1:7878")?;
-    let clients: Arc<Mutex<HashMap<usize, TcpStream>>> = Arc::new(Mutex::new(HashMap::new()));
-    let mut client_id = 0;
+    //let listener = TcpListener::bind("127.0.0.1:7878")?;
 
-    println!("Listening for clients...");
+    println!("Write the ip:port to bind to:");
+    let mut input_= String::new();
+    io::stdin().read_line(&mut input_)?;
 
-    for stream in listener.incoming() {
-        let stream = stream.expect("failed");
-        let clients_copy = Arc::clone(&clients);
-        clients.lock().unwrap().insert(client_id, stream.try_clone().unwrap());
+    let input_server = input_.trim().to_string();
+    let server_address = input_server.clone();
 
-        let handle = thread::spawn(move || {
-            handle_client(stream, client_id, clients_copy)
-                .unwrap_or_else(|error| eprintln!("{:?}", error));
-        });
+    let socket = UdpSocket::bind(input_server).expect("Failed to bind socket");
+    let mut buf = [0u8; 65507];
 
-        client_id += 1;
-    }
+    let server_addresses = &[
+        "0.0.0.0:8888", 
+        "0.0.0.0:8889", 
+        "0.0.0.0:8887", 
+    ];
 
+    let shared_directory = "shared_directory"; // Directory for shared coordination.
+
+    // Create a directory for coordination (if it doesn't exist).
+    create_dir_all(shared_directory).expect("Failed to create shared directory");
+
+
+    let next_server = match server_addresses.iter().position(|&addr| addr == server_address) {
+        Some(index) => (index + 1) % server_addresses.len(),
+        None => {
+            eprintln!("Server address not found in the list.");
+            return Ok(());
+        }
+    };
+
+
+    let next_server_address: String = server_addresses[next_server].parse().expect("Invalid server address");
+
+    let handle = thread::spawn(move || {
+        println!("Listening for clients...");
+        loop {
+            match socket.recv_from(&mut buf) {
+                Ok((amt, src)) => {
+                    println!("Received {} bytes from: {}", amt, src);
+                    if let Err(err) = write_image_to_file(&buf[..amt], &src) {
+                        eprintln!("Error writing image to file: {}", err);
+                    }
+
+                    // Check if this server is responsible for saving the image.
+                    let current_time = Utc::now().format("%Y-%m-%d %H-%M-%S").to_string();
+                    let coordination_file_path = format!("{}/coordination.txt", shared_directory);
+
+                    if !File::open(&coordination_file_path).is_ok() {
+                        // Save the image only if the coordination file doesn't exist.
+                        if let Err(err) = File::create(&coordination_file_path) {
+                            eprintln!("Error creating coordination file: {}", err);
+                        }
+
+                        // Relay the image data to the next server in the ring.
+                        if let Err(err) = socket.send_to(&buf[..amt], &next_server_address) {
+                            eprintln!("Error relaying image data to the next server: {}", err);
+                        }
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Error receiving data: {}", e);
+                }
+            }
+        }
+    });
+
+    // Wait for the handling thread to finish.
+    handle.join().expect("Thread join failed");
     println!("Shutting down server...");
 
     Ok(())
